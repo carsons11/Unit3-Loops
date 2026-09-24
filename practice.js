bb2f4edbae6e11835bbdea80508f3124e68d1a88
carsons11 <carsons11@nycstudents.net>
2026-09-24 12:34:35 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -25,7 +25,7 @@ console.log(sumRange(1, 5));   // 15
 console.log(sumRange(1, 100)); // 5050
 console.log(sumRange(4, 4));   // 4 */
 
-function countdown(n) {
+/* function countdown(n) {
   // TODO: your code here
   while (n > 0) {
   console.log("Countdown: " + n);  
@@ -36,6 +36,24 @@ function countdown(n) {
 console.log("Blast off!");
 }
 
-console.log(countdown(5)); // [5, 4, 3, 2, 1]
-/* console.log(countdown(1)); // [1]
-console.log(countdown(8)); // [8, 7, 6, 5, 4, 3, 2, 1] */
+countdown(5); // [5, 4, 3, 2, 1]
+countdown(1); // [1]
+countdown(8); // [8, 7, 6, 5, 4, 3, 2, 1,] */
+
+function countVowels(str) {
+  // TODO: your code here
+  x = str.length
+  y = 0
+    while (x>=0){
+      if (String(str[x]) === "a" || String(str[x]) === "e" || String(str[x]) === "i" || String(str[x]) === "o" || String(str[x]) === "u" ){
+        y++;
+      }
+      x--;
+    }
+  return y
+}
+
+console.log(countVowels("hello"));      // 2
+console.log(countVowels("javascript")); // 3
+console.log(countVowels("xyz"));        // 0
+console.log(countVowels("aeiou"));      // 5
